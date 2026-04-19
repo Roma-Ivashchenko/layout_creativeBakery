ef3957c2f2c7048c1970ee29250a445957bb5bda
Roma Ivashchenko <rrrrgvvggh@gmail.com>
2026-04-19 19:17:33 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+const menu = document.querySelector('#mobile-menu');
+const openBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
+const menuLinks = document.querySelectorAll('.menu__link');
+
+openBtn.addEventListener('click', () => {
+  menu.classList.add('menu--is-open');
+  document.body.style.overflow = 'hidden';
+});
+
+closeBtn.addEventListener('click', () => {
+  menu.classList.remove('menu--is-open');
+  document.body.style.overflow = '';
+});
+
+menuLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('menu--is-open');
+    document.body.style.overflow = '';
+  });
+});
